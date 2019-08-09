57820919869a66cb8c2ced1701c256f7232d8113
Atul tiwari <40922969+iamaeytee@users.noreply.github.com>
2019-08-09 18:14:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
-const express = require ('express')
-require('./db/mongoose1')
-const UserData = require('./schemas/users')
+const express = require('express')
 
-const app= express()
+require('./dbs/mongoose')
+const userData = require('./schemas/usersShema')
+
+const app = express()
+
+app.use(express.json())
 
 const port = process.env.PORT || 8080
 
-app.use(express.json());
-
 app.post('/users',(req,res)=>{
-   const user = new UserData(req.body)
-   user.save().then(()=>{
-       res.send(user)
-   }).catch((error)=>{
-       console.log(error)
-   })
+
+    const user = new userData(req.body)
+
+    user.save().then(()=>{
+        console.log(user);
+        res.send(user)
+    }).catch((error)=>{
+        res.status(400).send(error)
+        console.log(error)
+    })
 })
 
+app.get('/users',(req,res)=>{
+
+    userData.find({}).then((userData)=>{
+        res.send(userData)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+
+    userData.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
 app.listen(port,()=>{
     console.log('Server listening on port',port);
 })
